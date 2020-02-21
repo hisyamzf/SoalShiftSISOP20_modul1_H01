9347aaa7ddb07868dc3a6689fb182470eb99a03d
hisyamzf <55182043+hisyamzf@users.noreply.github.com>
2020-02-21 05:45:45 -0500

    Update soal3a.sh

--- soal3a.sh
+++ soal3a.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-for i in {1..28} do wget -O pdkt_kusuma_$i https://loremflickr.com/320/240/cat -a wget.log done
+for i in {1..28}
+do wget -O pdkt_kusuma_$i https://loremflickr.com/320/240/cat -a wget.log
+done
+
